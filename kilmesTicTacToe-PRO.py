307392e88785fbfa37c507a74d1afac979aa8a5d
Reprak11 <reynaldo.armenta@cetys.edu.mx>
2020-08-19 23:27:26 -0700

    Avance 19/08/2020 dificultad sencilla

--- kilmesTicTacToe-PRO.py
+++ kilmesTicTacToe-PRO.py
@@ -1,6 +1,20 @@
+import random
+
 class jugador:
     def __init__(self, name, estado, turno):
         self.name = name
+        self.estado = estado
+        self.turno = turno
+    def regresaTurno(self):
+        return self.turno
+    def regresaEstado(self):
+        return self.estado
+    def regresaName(self):
+        return self.name
+
+class pc:
+    def __init__(self, estado, turno):
+        self.name = 'miniT4ynis'
         self.estado = estado
         self.turno = turno
     def regresaTurno(self):
@@ -16,6 +30,8 @@ class tablero:
         self.muestraTablero()
         self.cuenta = 0
         self.final = False
+    def setModoJuego(self,formaJuego):
+        self.formaJuego = formaJuego
     def muestraTablero(self):
         for i in range(0,len(self.tablero)):
             fila=""
@@ -40,7 +56,7 @@ class tablero:
         except ValueError:
                 print("Valor fuera de rango")
                 self.posiX()
-    def PosiY(self):
+    def posiY(self):
         try:
             y = int(input("Ingresa Valor Y: "))
             if (y == 0 or y == 1 or y == 2):
@@ -48,22 +64,35 @@ class tablero:
             else:
                 print("Valor fuera de rango")
                 y = self.posiY()
-                return x
+                return y
         except ValueError:
                 print("Valor fuera de rango")
                 self.posiY()
-    
+    def randomPos(self):
+        return random.randint(0,2)
+
     def agregaEstado(self,estado, jugador):
-        print("Turno de "+jugador)
-        self.x = self.posiX()
-        self.y = self.PosiY()
-        if self.tablero[self.x][self.y] == " ":
-            self.tablero[self.x][self.y] = estado
-            self.muestraTablero()
-            self.checaGanador(self.x,self.y)
+        if(self.formaJuego == 1 and jugador == "miniT4ynis"):
+            print("Hola")
+            self.x = self.randomPos()
+            self.y = self.randomPos()
+            if self.tablero[self.x][self.y] == " ":
+                self.tablero[self.x][self.y] = estado
+                self.muestraTablero()
+                self.checaGanador(self.x,self.y)
+            else:
+                self.agregaEstado(estado,jugador)
         else:
-            print("No seas wey esa casilla ya está ocupada")
-            self.agregaEstado(estado,jugador)
+            print("Turno de "+jugador)
+            self.x = self.posiX()
+            self.y = self.posiY()
+            if self.tablero[self.x][self.y] == " ":
+                self.tablero[self.x][self.y] = estado
+                self.muestraTablero()
+                self.checaGanador(self.x,self.y)
+            else:
+                print("No seas wey esa casilla ya está ocupada")
+                self.agregaEstado(estado,jugador)
         
     def checaGanador(self, x,y):
             if (self.tablero[x][0] != " " and  self.tablero[x][0] == self.tablero[x][1] and self.tablero[x][1] == self.tablero[x][2]):
@@ -82,24 +111,47 @@ class tablero:
         return self.final
     def changeWinner(self):
         self.final = not self.final
-if __name__ == "__main__":
-    player1=jugador(input("Nombre del Jugador 1: "),"O", True)
-    player2=jugador(input("Nombre del Jugador 2: "),"X", False)
-    tabl = tablero()
-    turno = True
-    while (tabl.regresaCuenta() < 9):
-        if(turno == player1.regresaTurno()):
-            tabl.agregaEstado(player1.regresaEstado(), player1.regresaName())
+
+class Juego:
+    def __init__(self):
+        self.tabl = tablero()
+        self.turno = True
+        self.formaJuego=self.modoJuego()
+        self.tabl.setModoJuego(self.formaJuego)
+        self.player1 = jugador(input("Nombre Player 1: "),'O',True)
+        if (self.formaJuego == 1):
+            self.player2 = pc('X',False)
         else:
-            tabl.agregaEstado(player2.regresaEstado(), player2.regresaName())
-        if(tabl.regresaFinal()==True):
-            if (turno == True):
-                print(player2.regresaName()+" eres el rival mas debil, Adios")
+            self.player2 = jugador(input("Nombre Player 2: "),'X',False)
+        print(self.player1.regresaName())
+        print(self.player2.regresaName())
+        self.juegoTicTacToe()
+    def modoJuego(self):
+        try:
+            formaJuego = int(input("Modo de Juego\n1:Player vs PC\n2:Player vs Player\n"))
+            if (formaJuego != 1 and formaJuego != 2):
+                formaJuego = self.modoJuego()
+            return formaJuego
+        except ValueError:
+            print("Valor fuera de rango")
+            self.modoJuego()
+    def juegoTicTacToe(self):
+        while (self.tabl.regresaCuenta() < 9):
+            if(self.turno == self.player1.regresaTurno()):
+                self.tabl.agregaEstado(self.player1.regresaEstado(), self.player1.regresaName())
             else:
-                print(player1.regresaName()+" eres el rival mas debil, Adios ")
-            break
-        turno = not turno
-        tabl.agregaCuenta()
-    if (tabl.checaGanador == False):
-        print("Nadie Ganó")    
-    
+                self.tabl.agregaEstado(self.player2.regresaEstado(), self.player2.regresaName())
+            if(self.tabl.regresaFinal()==True):
+                if (self.turno == True):
+                    print(self.player2.regresaName()+" eres el rival mas debil, Adios")
+                else:
+                    print(self.player1.regresaName()+" eres el rival mas debil, Adios ")
+                break
+            self.turno = not self.turno
+            self.tabl.agregaCuenta()
+        if (self.tabl.checaGanador == False):
+            print("Nadie Ganó")
+
+
+if __name__ == "__main__":
+    juegoGato = Juego()    
